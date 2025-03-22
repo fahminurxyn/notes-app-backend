c54535c3c483ce824776d9cb228fba40c05453c1
Fahminur12 <fahmisenp@gmail.com>
2025-03-22 16:56:13 +0700

    Adding Postman Collection

--- src/handler.js
+++ src/handler.js
@@ -85,18 +85,41 @@ const editNoteByIdHandler = (request, h) => {
       body,
       updatedAt,
     };
+
+    const response = h.response({
+      status: "success",
+      message: "Catatan berhasil diperbarui",
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: "fail",
+    message: "Gagal memperbarui catatan. Id tidak ditemukan",
+  });
+  response.code(404);
+  return response;
+};
+
+const deleteNoteByIdHandler = (request, h) => {
+  const { id } = request.params;
  
+  const index = notes.findIndex((note) => note.id === id);
+ 
+  if (index !== -1) {
+    notes.splice(index, 1);
     const response = h.response({
       status: 'success',
-      message: 'Catatan berhasil diperbarui',
+      message: 'Catatan berhasil dihapus',
     });
     response.code(200);
     return response;
   }
  
-  const response = h.response({
+const response = h.response({
     status: 'fail',
-    message: 'Gagal memperbarui catatan. Id tidak ditemukan',
+    message: 'Catatan gagal dihapus. Id tidak ditemukan',
   });
   response.code(404);
   return response;
@@ -107,4 +130,5 @@ module.exports = {
   getAllNotesHandler,
   getNoteByIdHandler,
   editNoteByIdHandler,
+  deleteNoteByIdHandler,
 };
